2ad43e1d8319a20be578d84d7ad29ab0c5a698bf
Malik <malikfelix50@gmail.com>
2025-10-11 17:32:03 -0500

    Add TODO for Twitter Controller

--- app/controllers/api/v1/twitter_controller.rb
+++ app/controllers/api/v1/twitter_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::TwitterController < ApplicationController
   def recent
-    service = TwitterService.new("_malikfelix")
+    service = TwitterService.new("_malikfelix") # TODO: Make this dynamic
     tweets = service.get_tweets
     render json: tweets
   end
